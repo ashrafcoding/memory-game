8d10858b37fa13c47cc9b28c842473ba39a9780a
ashraf <ashrafeid@outlook.com>
2021-04-18 04:52:17 +0300

    adding functionality of stop click

--- main.js
+++ main.js
@@ -13,6 +13,66 @@ let blocksContainer = document.querySelector(".memory-game-blocks")
 let blocks = Array.from(blocksContainer.children)
 let orderRange = [...Array(blocks.length).keys()]
 
+shuffle(orderRange)
+
+
 blocks.forEach((block, index) => {
-    block.style.order = index
+    block.style.order = orderRange[index]
+    block.addEventListener('click', function(){
+      flipBlock(block)
+    })
 })
+
+// flip block function
+function flipBlock(selectedBlock){
+  selectedBlock.classList.add('is-flipped')
+
+  // collect all flipped cards
+  let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'))
+  if(allFlippedBlocks.length === 2){  
+    stopClicking()
+    checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1])
+  }
+}
+
+// stop clicking function
+function stopClicking(){
+  blocksContainer.classList.add('no-clicking')
+
+  setTimeout(()=>{
+    blocksContainer.classList.remove('no-clicking')
+  },duration)
+}
+
+// check matched blocks
+function checkMatchedBlocks(firstBlock, secondBlock){
+  let triesElement = document.querySelector('.tries span')
+  if(firstBlock.dataset.technology == secondBlock.dataset.technology){
+    firstBlock.classList.remove('is-flipped')
+    secondBlock.classList.remove('is-flipped')
+
+    firstBlock.classList.add('has-match')
+    secondBlock.classList.add('has-match')
+    document.getElementById('success').play()
+  } else {
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1
+    setTimeout(()=>{
+      firstBlock.classList.remove('is-flipped')
+      secondBlock.classList.remove('is-flipped')
+    },duration)
+    document.getElementById('fail').play()
+
+  }
+}
+// shuffle function
+function shuffle(array){
+  let current = array.length , temp, random
+  while(current > 0){
+    random = Math.floor(Math.random()*current)
+    current --
+    temp = array[current]
+    array[current] = array[random]
+    array[random] = temp
+  }
+  return array
+}
